1acf4dc61e522c02488cff4287ce9059ce980e98
jeroenflvr <jeroen@flexworks.eu>
2025-02-01 09:30:36 +0100

    chore: remove maturin init example code

--- crates/pyaxp/src/lib.rs
+++ crates/pyaxp/src/lib.rs
@@ -2,12 +2,6 @@ use pyo3::prelude::*;
 use yaxp_common::xsdp::parser::parse_file;
 
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 #[pyfunction]
 fn parse_xsd_to_json(py: Python, xsd_file: &str) -> PyResult<PyObject> {
     let result = parse_file(xsd_file);
@@ -23,10 +17,9 @@ fn parse_xsd_to_json(py: Python, xsd_file: &str) -> PyResult<PyObject> {
     }
 }
 
-/// A Python module implemented in Rust.
+// main entrypoint for python module
 #[pymodule]
 fn pyaxp(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(parse_xsd_to_json, m)?)?;
     Ok(())
 }
