fb3004606a79369dec4670fcb6a5542cfa5b56f6
jeroenflvr <jeroen@flexworks.eu>
2025-02-01 09:31:47 +0100

    refactor: into workspace for lib, cli and python bindings

---
+++ crates/yaxp-cli/src/main.rs
@@ -0,0 +1,16 @@
+use yaxp_common::xsdp::parser::parse_file;
+
+fn main() {
+    let result = parse_file("../../example.xsd");
+
+    match result {
+        Ok(schema) => {
+            schema.write_to_file("../../example-output.json").unwrap();
+            let arrow_schema = schema.to_arrow().unwrap();
+            dbg!(arrow_schema);
+            println!("✅ Done!")
+        }
+        Err(e) => eprintln!("❌ {}", e),
+    }
+
+}
